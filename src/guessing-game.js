24da4cbf9a3b66e589bd77ec12dd2efa6fbcf2b3
shchegolenkov <denis.amorales@ya.ru>
2023-02-27 18:38:51 +0300

    feat: guessing-game done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.try = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.try = Math.round((this.max + this.min) / 2);
+    return this.try;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.try;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.try;
+  }
 }
 
 module.exports = GuessingGame;
